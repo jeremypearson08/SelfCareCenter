e5a4cb66e329a3b2984784288a8dba474d91ce8d
Jeremy Pearson <jeremy.pearson08@gmail.com>
2023-10-29 15:36:26 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//Variables
+var receiveMessageBtn = document.querySelector('.receive-button');
+var affirmationRadio = document.querySelector('.affirmation-container');
+var mantraRadio = document.querySelector('.mantra-container');
+var resultsContainer = document.querySelector('.results-container');
+
+//Event Listeners
+receiveMessageBtn.addEventListener('click', displayRandomMessage);
+
+//Event Handlers and Functions
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  function hide(element) {
+    element.style.display = 'none';
+  }
+
+  function displayRandomMessage() {
+    var selectedArray;
+    if (affirmationRadio.querySelector('input').checked) {
+      selectedArray = affirmations;
+    } else {
+      selectedArray = mantras;
+    }
+    var randomIndex = getRandomIndex(selectedArray);
+    var randomMessage = selectedArray[randomIndex];
+    resultsContainer.innerHTML = randomMessage;
+    var meditationSymbol = document.getElementById('meditate');
+    if (meditationSymbol) {
+        hide(meditationSymbol);  
+        }
+    }
